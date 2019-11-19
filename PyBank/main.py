4791a36666ce92da1e20a0be35ebccc04887c19d
Ann Asiatico <annasiatico@gmail.com>
2019-11-19 17:56:17 -0600

    updated main.py and add output file

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,11 +10,8 @@ with open(filepath, "r") as in_file:
 
     l = list(in_csv)
 
-    print ("Financial Analysis")
-    print ("---------------------------")
-
+    #total count
     count_of_row = len(l)
-    print ("Total Months: " + str(count_of_row))
 
     #splitting the list into two lists: date and profit/losses
     date = [i[0] for i in l]
@@ -22,18 +19,12 @@ with open(filepath, "r") as in_file:
     
     # using list comprehension to perform convert string element to integer to get the total
     int_prof_loss = [int(i) for i in prof_loss] 
-    print ("Total: " + "$" + str(sum(int_prof_loss)))
-
-    #merging two lists into one list
-    # new_l = zip(date, int_prof_loss)
-    # new_list = list(new_l)
     
     #compute for average change
     avg_change = [int_prof_loss[i+1]-int_prof_loss[i] for i in range(len(int_prof_loss)-1)]
     def Average(lst): 
         return sum(lst) / len(lst) 
     average = Average(avg_change)
-    print ("Average Change: " + "$" + str(format(average, '.2f')))
 
     #merging two lists into one list starting at index 1 for date because index 0 of 
     # avg_change is null
@@ -49,8 +40,29 @@ with open(filepath, "r") as in_file:
     increase = new_list[-1]
     decrease = new_list[0]
 
+    #export results to terminal and a text file
+    output = open('output.txt', 'w')
+
+    print ("Financial Analysis")
+    print ("Financial Analysis", file = output)
+
+    print ("---------------------------")
+    print ("---------------------------", file = output)
+
+    print ("Total Months: " + str(count_of_row))
+    print ("Total Months: " + str(count_of_row), file = output)
+
+    print ("Total: " + "$" + str(sum(int_prof_loss)))
+    print ("Total: " + "$" + str(sum(int_prof_loss)), file = output)
+
+    print ("Average Change: " + "$" + str(format(average, '.2f')))
+    print ("Average Change: " + "$" + str(format(average, '.2f')), file = output)
+
     print ("Greatest Increase in Profits: " + str(increase[0]) + " " + "($" + str(increase[1]) + ")")
+    print ("Greatest Increase in Profits: " + str(increase[0]) + " " + "($" + str(increase[1]) + ")", file = output)
+
     print ("Greatest Increase in Profits: " + str(decrease[0]) + " " + "($" + str(decrease[1]) + ")")
+    print ("Greatest Increase in Profits: " + str(decrease[0]) + " " + "($" + str(decrease[1]) + ")", file = output)
 
     
     
