a46d94a42c6c3a8e915891f8049a974eafb30ea0
Ann Asiatico <annasiatico@gmail.com>
2019-11-19 09:56:26 -0600

    edited main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,5 @@
 import os
 import csv
-from pprint import pprint
-
 
 filepath = os.path.join("budget_data.csv")
 
@@ -9,7 +7,6 @@ with open(filepath, "r") as in_file:
     in_csv = csv.reader(in_file)
 
     header = next(in_csv)
-
 
     l = list(in_csv)
 
@@ -28,8 +25,8 @@ with open(filepath, "r") as in_file:
     print ("Total: " + "$" + str(sum(int_prof_loss)))
 
     #merging two lists into one list
-    new_l = zip(date, int_prof_loss)
-    new_list = list(new_l)
+    # new_l = zip(date, int_prof_loss)
+    # new_list = list(new_l)
     
     #compute for average change
     avg_change = [int_prof_loss[i+1]-int_prof_loss[i] for i in range(len(int_prof_loss)-1)]
@@ -38,7 +35,8 @@ with open(filepath, "r") as in_file:
     average = Average(avg_change)
     print ("Average Change: " + "$" + str(format(average, '.2f')))
 
-    #merging two lists into one list
+    #merging two lists into one list starting at index 1 for date because index 0 of 
+    # avg_change is null
     new_l = zip(date[1:], avg_change)
     new_list = list(new_l)
     
@@ -47,6 +45,7 @@ with open(filepath, "r") as in_file:
         return element2[1]
     new_list.sort(key=sortlist)
 
+    #create new lists to get the required output format
     increase = new_list[-1]
     decrease = new_list[0]
 
